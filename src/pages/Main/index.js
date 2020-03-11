29ae9446054294ed2e36b1127f34314e3cd4469d
gabrielrsp <gabrielrsp18@gmail.com>
2020-03-11 17:37:29 -0300

    trying to implement useEffect for add books feature

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { BookList, StyledLink, Container, Form, SubmitButton  } from './styles';
 
 import { FaPlus } from "react-icons/fa";
 
 function Main() {
+
+  const [book, setBook] = useState([]);
+
+  const [newBook, setNewBook] = useState([]);
+
+  function handleAdd () {
+    setBook([ ...[...book], [newBook] ]);
+    setNewBook([]);
+  };
+
+  useEffect(() => {
+    const data = localStorage.getItem('book-list');
+    if(data){
+      setBook(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('book-list', JSON.stringify(book));
+  }, [book]);
+
   return (
     <>
 
@@ -14,22 +35,34 @@ function Main() {
         <input
           type="text"
           placeholder="Book Name"
+          value={newBook.name}
+          name="name"
+          onChange={e => setNewBook(e.target.value)}
+
         />
         <input
           type="text"
           placeholder="Author"
+          value={newBook.author}
+          name="author"
+          onChange={e => setNewBook(e.target.value)}
         />
         <input
           type="text"
           placeholder="URL image"
+          value={newBook.url}
+          name="url"
+          onChange={e => setNewBook(e.target.value)}
         />
         <textarea
           type="text"
           placeholder="Notes"
+          value={newBook.notes}
+          name="notes"
+          onChange={e => setNewBook(e.target.value)}
         />
-        <SubmitButton disabled  >
+        <SubmitButton onClick={handleAdd}  >
           <FaPlus color='#fff' size={22} />
-
           <span>Add Book</span>
         </SubmitButton>
       </Form>
@@ -38,78 +71,94 @@ function Main() {
 
 
     <BookList>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
-      <li>
-        <a href="details">
-          <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
-          alt="book"/>
-        </a>
-        <strong>a real good chinese book</strong>
-        <button type="button">
-        <StyledLink to="/details" >
-        <span>Details</span>
-        </StyledLink>
-        </button>
-      </li>
+        {
+          book.map( book => (
+            <li key={book}>
+              <a href="details">
+                <img src={book.url} alt="book"/>
+                <strong>{book.name}</strong>
+                <button type="button">
+                  <StyledLink to="/details" >
+                  <span>Details</span>
+                  </StyledLink>
+                </button>
+              </a>
+            </li>
+          ))
+        }
+
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
+        <li>
+          <a href="details">
+            <img src="https://m.media-amazon.com/images/I/81kB6lKq1aL._AC_UY218_ML3_.jpg"
+            alt="book"/>
+          </a>
+          <strong>a real good chinese book</strong>
+          <button type="button">
+          <StyledLink to="/details" >
+          <span>Details</span>
+          </StyledLink>
+          </button>
+        </li>
 
       </BookList>
 
