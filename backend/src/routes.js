1b3fa97d0c326711539fb484538f7315d74301e2
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-06 17:15:35 -0300

    add security features to request on api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 import Router from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -14,8 +16,15 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store)
+routes.post('/sessions', bruteForce.prevent, SessionController.store);
 
 routes.use(authMiddleware); //global middleware valid only for routes below
 
